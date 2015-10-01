df54ea433d76e1c404bd428e2b2191709ffbd63e
gleis <gleis@Win10>
2015-09-30 21:27:45 -0300

    Criação da classe para teste de conexão com Banco de Dados

---
+++ TrabalhoBootstrap/TrabalhoBootstrap/src/main/java/view/TestaConexao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controle.Conexao;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Gleison
+ */
+public class TestaConexao {
+
+    public static void main(String[] args) {
+        Connection conexao = Conexao.getConexao();
+        Conexao.fechaConexao(conexao);
+        System.out.println("Conectado com Sucesso!");
+    }
+}
